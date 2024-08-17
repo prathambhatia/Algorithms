c7a4e18dbcee50b90c8e91be55b68641330ad9ad
Pratham Bhatia <83857889+prathambhatia@users.noreply.github.com>
2024-08-18 03:22:55 +0530

    Update Bidirectional Max-Tracker Algorithm.py

--- Bidirectional Max-Tracker Algorithm.py
+++ Bidirectional Max-Tracker Algorithm.py
@@ -7,7 +7,7 @@ class Solution:
     Computes the maximum trapped water between bars of varying heights in a given array using a two-pointer approach.
      Returns:
         int: The total amount of trapped water.
-        Time Complexity , Space Complexity = O(N),O(1)))))
+        Time Complexity , Space Complexity = O(N),O(1))))))
     """
         if not height: return 0
 
